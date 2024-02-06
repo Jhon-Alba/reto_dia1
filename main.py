9385b7383c70f95002b46fd214171d5f0bb5e597
Jhon Deivy Alba Bocachica <siganadeivyalba@gmail.com>
2024-02-05 20:18:02 -0500

    Finalizado

---
+++ main.py
@@ -0,0 +1,14 @@
+
+def reto_dia1():
+    nombre = input("Ingrese su(s) nombre(s): ")
+    apellido = input("Ingrese sus apellidos: ")
+    numero= input("Ingrese su numero de telefono: ")
+    correo = input("Ingrese su direccion de correo electronico: ")
+
+    print(f"Hola {nombre }{apellido}, en breve recibiras un correo electronico a {correo}")
+
+
+if __name__ == '__main__':
+    reto_dia1()
+
+
